6510cbdc6160294598a1dd1b350136d33d4cd807
Anant1004 <anant1042005@gmail.com>
2025-09-20 12:29:16 +0530

    rect:: midlldeware added for authentication

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+
+  const { pathname } = req.nextUrl;
+
+  if (pathname.startsWith("/admin") && !token) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (pathname === "/" && token) {
+    return NextResponse.redirect(new URL("/admin", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/admin/:path*"], 
+};
